bd262c3f9fdf908ee9cffcdf80e631c96ce2a54f
JuanDRosero <43081291+JuanDRosero@users.noreply.github.com>
2020-08-02 17:45:34 -0500

    Ya esta eliminar :v

--- ArbolRN.h
+++ ArbolRN.h
@@ -13,6 +13,7 @@ class arbolRN{Nodo *raiz;
     pila<int> *listInorden;
     pila<int> *listPreorden;
 	pila<int> *listPosorden;
+	Nodo* buscarNodo(int clave);
     public: arbolB(){raiz = NULL;
     					listInorden = new pila<int>; 
                         listPreorden = new pila<int>;
@@ -21,7 +22,6 @@ class arbolRN{Nodo *raiz;
         void insertar(int dato);
         Nodo *buscarpadre(int dato);
         void eliminar(int dato);
-        Nodo *buscar_nodo(int dato);
         void inorden();
         void preorden();
         void posorden();
@@ -30,7 +30,9 @@ class arbolRN{Nodo *raiz;
         void imprimir();
        	~arbolB();
 };
-
+Nodo* arbolesRN::buscarNodo(int clave){
+	//Falta esta verga
+}
 void arbolRN::insertar(int dato){
 	Nodo *arb,*pre, *aux = new Nodo;
 	aux->info=dato;
@@ -185,26 +187,101 @@ Nodo *arbolRN::buscar_nodo(int dato){
 	
 }
 void arbolRN::eliminar(int dato){
-	Nodo *eliminado= buscar_nodo(dato);
-	Nodo *padre, *padreR, *hijoR =NULL;
+	Nodo *eliminado= buscar_nodo(dato),aux1,w;
+	Nodo *padre,padre1, *padreR, *hijoR =NULL;
+	int valor;
+	aux1=eliminado;
+	//		Ajustar SupreionRN
+		while(aux1 != raiz and aux->color==true){	//Recordar True es negro y false es Rojo	Y aux1 es x en el pseudocodigo
+			padre1=buscarpadre(aux1->info);
+			if(padre1->izq==aux1){
+				w=padre1->der;		//Que hacer si es nulo?
+				if(w->color==false){
+					//Caso 1
+					w->color=true;
+					padre1->color=false;
+					rotarIzquierda(padre1->info);
+					w=padre1->der;
+				}
+				if((w->izq==NULL || w->izq->color==true) and (w->der==NULL || w->der->color==true)){	//Los nodos hoja tiene hijos negros (por eso el null)
+					//Caso 2
+					w->color=false;
+					aux1=padre1;
+				} else {
+					if(w->der== NULL || w->der->color==true){
+						//Caso 3
+						w->izq->color=true;
+						w->color=false;
+						rotarDerecha(w->info);
+						w=padre1->der;
+					}
+					//Caso 4
+					w->color=padre1->color;
+					padre1->color=true;
+					w->der->color=true;
+					rotarIzquierda(padre1->info);
+					aux1=raiz;
+				}
+			}
+			else {
+				//Codigo simetrico
+				w=padre1->izq;		//Que hacer si es nulo?
+				if(w->color==false){
+					//Caso 1 Simetrico
+					w->color=true;
+					padre1->color=false;
+					rotarDerecha(padre1->info);
+					w=padre1->izq;
+				}
+				if((w->izq==NULL || w->izq->color==true) and (w->der==NULL || w->der->color==true)){	//Los nodos hoja tiene hijos negros (por eso el null)
+					//Caso 2 Simetrico
+					w->color=false;
+					aux1=padre1;
+				} else {
+					if(w->izq== NULL || w->izq->color==true){
+						//Caso 3 Simetrico
+						w->der->color=true;
+						w->color=false;
+						rotarIzquierda(w->info);
+						w=padre1->izq;
+					}
+					//Caso 4 Simetricop
+					w->color=padre1->color;
+					padre1->color=true;
+					w->izq->color=true;
+					rotarDerecha(padre1->info);
+					aux1=raiz;
+				}
+				
+			}
+		}
+		aux1->color=true;
+	//
+	
 	//cout<<eliminado->info<<endl;
 	if(eliminado->der == NULL && eliminado->izq == NULL){
-		//padre= buscar padre(dato o eliminado);
+		padre= buscar padre(dato);
 		if(eliminado->info > padre->info){
 			padre->der = NULL;
 		}else{
 			padre->izq = NULL;
 		}
 	}else if(eliminado->der != NULL && eliminado->izq == NULL){
-		padre->der = eliminado->der;
-		delete eliminado;
+		eliminado->info=eliminado->der->info;
+		eliminado->der=NUll;
 	} else if(eliminado->der == NULL && eliminado->izq != NULL){
-		padre->izq = eliminado->izq;
-		delete eliminado;
+		eliminado->info=eliminado->izq->info;
+		eliminado->izq=NULL;
 	}else  if(eliminado->der != NULL && eliminado->izq != NULL){
-		
+		Nodo *aux=eliminado->der;
+		while(aux->izq != NULL){	//Busca el nodo de la derecha mas a la izquierda
+			aux=aux->izq;
+		}
+		valor=aux->info;			//Guarda el valor del nodo de la derecha mas de la izquierda
+		eliminar(valor);			//Elimina el nodo de la derecha mas a la izquierda
+		eliminado->info=valor;		//Intercambia la clave con el nodo eliminado
 	}
-	}
+}
 
 void arbolRN::rotarIzquierda(int clave){
 	Nodo *auxP=NULL, *aux=raiz,*aux2;
